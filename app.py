282f1b8cf6d2b33a02310543871fccf065d7eed9
A Greaves <anna.j.greaves@gmail.com>
2022-10-05 18:59:39 +0000

    Add base template and movie list page.

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect,
+    request, session, url_for
+)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNANE"] = os.environ.get("MONGO_DBNANE")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def index():
-    return "Hello world ... again!"
+@app.route('/get_movies')
+def get_movies():
+    movies = mongo.db.movies.find()
+    return render_template("movies.html", movies=movies)
 
 
 if __name__ == "__main__":
